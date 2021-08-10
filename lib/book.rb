bac33195ee49f66b4a2364abcd7c35b52dbc17b8
Whiting <whitinghtennis@gmail.com>
2021-08-10 07:18:01 -0700

    commit tree b27650133ded66c39600acd3ba965cdc32448f4d

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,26 @@
 
+class Book
+    attr_reader :title, :author, :page_count, :genre
 
+    def initialize(title = "And Then There Were None")
+        @title = title
+       
+    end
+
+    def author= author
+        @author = author
+    end
+
+    def page_count= page_count
+        @page_count = page_count
+    end
+
+    def genre= genre
+        @genre = genre
+    end
+
+    def turn_page 
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,18 @@
-# Make your shoe class here!
+class Shoe
+attr_accessor :brand, :color, :size, :material, :condition
+
+    def initialize(brand = "Adidas", color = "red", size = "9.5", material = "suede", condition = "tattered")
+        @brand = brand
+        @color = color
+        @size = size
+        @material = material
+        @condition = condition
+
+    end
+
+    def cobble 
+        self.condition = 'new'
+        puts "Your shoe is as good as new!"
+    end
+
+end
